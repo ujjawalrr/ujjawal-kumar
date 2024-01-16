519cfa78c470fdd242f939a9d101242f01b9e8e3
Ujjawal Kumar <ujjawalrr@gmail.com>
2024-01-17 04:35:31 +0530

    initial

---
+++ api/index.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from "cors";
+import nodemailer from 'nodemailer';
+
+dotenv.config();
+
+const app = express();
+
+const port = 3000;
+app.use(cors({
+    origin: ["http://127.0.0.1:5500/", "https://ujjawalrr.github.io", "https://ujjawalrr.github.io/ujjawal-kumar/"],
+}))
+app.listen(port, () => {
+    console.log("Server is running!");
+});
+
+app.use(express.json());
+
+app.post('/contact', async (req, res, next) => {
+    console.log(req.body)
+    res.status(200).json("Message sent!");
+    try {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'josaacounsellors@gmail.com',
+                pass: process.env.MAIL
+            }
+        })
+        const mailOptions = {
+            from: 'josaacounsellors@gmail.com',
+            to: "ujjawalrr@gmail.com",
+            subject: "Important Message From Portfolio Website",
+            html: `User <b>${req.body.name}</b> is trying to contact you via <b>${req.body.email}</b>. <br> <b>Message:</b> ${req.body.message}`
+        }
+        transporter.sendMail(mailOptions, (error, info) => {
+            res.status(200).json("Message sent!");
+        })
+    } catch (error) {
+        next(error);
+    }
+});
---
+++ js/script.js
@@ -0,0 +1,54 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    750: {
+      slidesPerView: 3,
+    },
+    950: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+let contactBtn = document.getElementById('contactBtn');
+async function handleFormSubmit(e) {
+  e.preventDefault();
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+  try {
+    const res = await fetch('http://localhost:3000/contact', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, message }),
+    })
+    const data = await res.json();
+    alert(data);
+  } catch (error) {
+    alert(error);
+  }
+}
+contactBtn.addEventListener('click', handleFormSubmit);
